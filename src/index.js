753879af190bf9b56b19711fd0d61e39706c21d0
Aakrit Subedi <aakritsubedi9@gmail.com>
2019-11-21 07:43:16 +0545

    Hello World Eg

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render( < App / > , document.getElementById('root'));
+const root = document.getElementById('root');
+const element = < h1 > Hello Aakrit Subedi < /h1>
 
-serviceWorker.unregister();
+ReactDOM.render(
+    element, root
+);
